5640e6b819882b32922dee5f4bc3412d0cfd51ec
Fredika Benz <benedictbenz.kerja@gmail.com>
2023-02-28 00:34:16 +0700

    membuat factory dan seeder serta inisialisasi api

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Company;
+use App\Models\Employee;
+use App\Models\Responsibility;
+use App\Models\Role;
+use App\Models\Team;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +19,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Company::factory(10)->create();
+        // Team::factory(5)->create();
+        // Role::factory(50)->create();
+        // Responsibility::factory(100)->create();
+        // Employee::factory(1000)->create();
+
     }
 }
